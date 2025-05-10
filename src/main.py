940b8ba03973b89b6cc0df1cc036e401a6c9d295
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-05-11 04:59:10 +0900

    fix: remove blank last line

--- src/main.py
+++ src/main.py
@@ -2,29 +2,33 @@ import os
 import time
 
 DATA_DIR = "."
-WORK_DIR = os.path.join(os.environ.get("USERPROFILE"),".aliyun-oss-log-parser")
+WORK_DIR = os.path.join(
+    os.environ.get("USERPROFILE"), ".aliyun-oss-log-parser"
+)
 DATA_FILE = "data.log"
+
+file_thershold = 0
 
 
 def get_working_dir():
-    env_path=os.path.join(WORK_DIR,".env")
+    env_path = os.path.join(WORK_DIR, ".env")
     print(env_path)
     if os.path.exists(env_path):
         with open(env_path, "r", encoding="utf-8") as f:
             DATA_DIR = f.read()
     return DATA_DIR
 
+
 def get_file_list(dir):
-    f_list_raw=os.listdir(dir)
-    f_list=[]
+    f_list_raw = os.listdir(dir)
+    f_list = []
     for i in f_list_raw:
 
-        if os.path.isdir(os.path.join(DATA_DIR,i)):
+        if os.path.isdir(os.path.join(DATA_DIR, i)):
             pass
         else:
             f_list.append(i)
     return f_list
-
 
 
 def timing_wrapper(func):
@@ -46,20 +50,24 @@ def main():
     file_total = len(file_list)
     print(f"[INFO] Total files: {file_total}")
 
-    file_thershold = 20
     log_data = []
-    for count, file_name in enumerate(file_list[:file_thershold], start=1):
+    for count_file, file_name in enumerate(
+        file_list[
+            : (file_thershold if file_thershold != 0 else len(file_list))
+        ],
+        start=1,
+    ):
         with open(
             os.path.join(DATA_DIR, file_name), "r", encoding="UTF-8"
         ) as file:
             raw_data = file.read().split("\n")
             # raw_data=list(filter(bool,raw_data))
-            log_data.extend(raw_data)
-            print(f"[READ]: ({count}/{file_total}) = {file_name}")
+            log_data.extend(raw_data[:-1])
+            print(f"[READ]: ({count_file}/{file_total}) = {file_name}")
 
     print(f"[INFO]: Total log lines = {len(log_data)}")
 
-    if os.path.exists(WORK_DIR)==False:
+    if os.path.exists(WORK_DIR) == False:
         os.mkdir(WORK_DIR)
     with open(
         os.path.join(WORK_DIR, DATA_FILE), "w", encoding="utf-8"
